eceba86db7302abaf9dd434f5ebc19808ae08540
rajulshakya777 <shakyawar.r@gmail.com>
2024-10-12 15:09:58 -0500

    testing the content of the file

--- hello_world_package/hello.py
+++ hello_world_package/hello.py
@@ -3,6 +3,7 @@ import os
 def say_hello():
     # Construct the file path
     file_path = os.path.join(os.path.dirname(__file__), 'data', 'sample.txt')
+    print("Constructed file path:", file_path)  # Debugging line
     
     # Read the content of the sample text file
     try:
